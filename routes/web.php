c9ff5750e84fa1e2b69b35f493e4f3123a96b939
Vika <vikapetrova8@gmail.com>
2026-06-09 23:44:27 +0300

    feat(routes): add application routes
    Register the services page and application listing and submission endpoints.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\ApplicationController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'services')->name('services');
+
+Route::post('/applications', [ApplicationController::class, 'store'])
+    ->name('applications.store');
+
+Route::get('/applications', [ApplicationController::class, 'index'])
+    ->name('applications.index');
